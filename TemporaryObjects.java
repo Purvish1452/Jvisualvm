6fb71829ba051e8961acbce96f31080a79daa37a
Purvish1452 <23it070@charusat.edu.in>
2026-07-22 12:40:40 +0530

    TemporaryObjects--Java

--- TemporaryObjects.java
+++ TemporaryObjects.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class TemporaryObjects {
+
+    public static void main(String[] args) {
+
+        while (true) {
+            for (int i = 0; i < 100000; i++) {
+                String s = new String("Hello");
+            }
+        }
+    }
 }
