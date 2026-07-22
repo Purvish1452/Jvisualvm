4b58f8d7c90ab63fc3f4a6a398e967447eb51734
Purvish1452 <23it070@charusat.edu.in>
2026-07-22 12:39:28 +0530

    StaticMemory--Java

--- StaticMemory.java
+++ StaticMemory.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class StaticMemory {
+
+    static byte[] data = new byte[100 * 1024 * 1024];
+
+    public static void main(String[] args) throws Exception {
+        Thread.sleep(600000);
+    }
 }
