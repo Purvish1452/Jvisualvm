17a4d963e529a8f12df1eb76ac67ec807e6d444e
Purvish1452 <23it070@charusat.edu.in>
2026-07-22 12:39:57 +0530

    StringPool--Java

--- StringPoolDemo.java
+++ StringPoolDemo.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class StringPoolDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        while (true) {
+            String s1 = "Java";
+            String s2 = new String("Java");
+        }
+    }
 }
