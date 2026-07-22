212065fd78405fca65f4500964325c5fad65c9f1
Purvish1452 <23it070@charusat.edu.in>
2026-07-22 11:20:32 +0530

    Memoryleak--Java

--- MemoryLeak.java
+++ MemoryLeak.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class MemoryLeak {
+
+    static List<Object> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        while (true) {
+            list.add(new byte[1024 * 1024]); // 1 MB
+        }
+    }
 }
